e0c5538a4e3ba2de128cdd819e5b0e099c2f2a70
overd <forresst@sheridancollege.ca>
2024-08-06 23:21:11 -0400

    Singleton Design pattern

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -1,36 +1,97 @@
+
 package ca.sheridancollege.project;
-
 /**
- * The class that models your game. You should create a more specific child of this class and instantiate the methods
- * given.
- * 
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ *
+ * @author jasme
+ * modified by Stefan
  */
-public abstract class Game {
-	private final String name;
-	public Player unnamed_Player_5;
-	public Result unnamed_Result_6;
-	public DeckOfCards unnamed_DeckOfCards_7;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
-	/**
-	 * the players of the game
-	 */
-	public Game(String aName) {
-		throw new UnsupportedOperationException();
-	}
+public class Game {
+    private PlayerDeck playerDeck;
+    private CompDeck compDeck;
+    private PlayerDiscard playerDiscard;
+    private CompDiscard compDiscard;
+    private PlayArea playArea;
+    private Scanner scanner;
 
-	/**
-	 * Play the game. This might be one method or many method calls depending on your game.
-	 */
-	public abstract void play();
+    // Singleton instance
+    private static Game instance;
 
-	/**
-	 * When the game is over, use this method to declare and display a winning player.
-	 */
-	public abstract void declareWinner();
+    // Private constructor to prevent instantiation
+    private Game(Scanner scanner) {
+        this.scanner = scanner; // Use the shared scanner instance
+        initializeGame();
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    // Public method to get the singleton instance
+    public static Game getInstance(Scanner scanner) {
+        if (instance == null) {
+            instance = new Game(scanner);
+        }
+        return instance;
+    }
+
+    private void initializeGame() {
+        playerDeck = new PlayerDeck();
+        compDeck = new CompDeck();
+        playerDiscard = new PlayerDiscard();
+        compDiscard = new CompDiscard();
+        playArea = new PlayArea(playerDeck, compDeck, playerDiscard, compDiscard);
+        initializeDecks();
+    }
+
+    private void initializeDecks() {
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        ArrayList<Card> fullDeck = new ArrayList<>();
+
+        for (String suit : suits) {
+            for (int value = 1; value <= 13; value++) {
+                fullDeck.add(new Card(suit, value));
+            }
+        }
+
+        Collections.shuffle(fullDeck);
+
+        for (int i = 0; i < fullDeck.size(); i++) {
+            if (i < 26) {
+                playerDeck.addCard(fullDeck.get(i));
+            } else {
+                compDeck.addCard(fullDeck.get(i));
+            }
+        }
+    }
+
+    public void startGame() {
+        boolean exit = false;
+
+        while (!exit) {
+            while (!playArea.isGameOver()) {
+                System.out.println("Press Enter to draw a card, or type 'exit' to quit.");
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("exit")) {
+                    exit = true;
+                    break;
+                }
+                playArea.compare();  // Handle card comparisons and rounds
+            }
+
+            if (!exit) {
+                System.out.println("Do you want to play again?");
+                System.out.println("1. Yes");
+                System.out.println("2. No");
+                System.out.print("Enter your choice: ");
+                int choice = scanner.nextInt();
+                scanner.nextLine();  // Consume the newline
+
+                if (choice == 1) {
+                    initializeGame();  // Restart the game
+                } else {
+                    exit = true; // Set exit to true to return to the main menu
+                }
+            }
+        }
+    }
 }
