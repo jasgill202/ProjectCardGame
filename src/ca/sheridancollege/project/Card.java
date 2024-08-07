f46b14bce11017b9045a0d92b52a8221e76ea507
overd <forresst@sheridancollege.ca>
2024-08-06 23:13:03 -0400

    Card updated

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -1,39 +1,30 @@
+
+
 package ca.sheridancollege.project;
-
 /**
- * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
- * game. Students wishing to add to the code should remember to add themselves as a modifier.
- * 
- * @author dancye
- * @modifier Jasmeet Gill
- * @modifier
+ *
+ * @author overd
+ * @modified by jasme
  */
-public abstract class Card {
-	private String suit;
-	private int value;
-	public DeckOfCards consists_of;
-	public DeckOfCards unnamed_DeckOfCards_33;
+public class Card {
+    private final String suit;
+    private final int value;
 
-	/**
-	 * Students should implement this method for their specific children classes
-	 * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-	 */
-	@Override
-	public abstract String toString();
+    public Card(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
 
-	public String getSuit() {
-		return this.suit;
-	}
+    public String getSuit() {
+        return suit;
+    }
 
-	public void setSuit(String aSuit) {
-		this.suit = aSuit;
-	}
+    public int getValue() {
+        return value;
+    }
 
-	public int getValue() {
-		return this.value;
-	}
-
-	public void setValue(int aValue) {
-		this.value = aValue;
-	}
+    @Override
+    public String toString() {
+        return value + " of " + suit;
+    }
 }
